58d2b5cab99950ba79127b89a7baef79aff84d01
E_BATUR <EBATUR@THY.COM>
2020-03-29 19:16:48 +0300

    add product service and configure product.service.ts

---
+++ src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+    sku: string;
+    name: string;
+    description: string;
+    unirPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+}
